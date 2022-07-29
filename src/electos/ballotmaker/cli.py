26081c168419d52b21b13021b0e3c825a2e2837e
Neil Johnson <neil@cadent.com>
2022-07-29 15:33:17 -0400

    Create make ballots interface w/ 100% pytest cover

--- src/electos/ballotmaker/cli.py
+++ src/electos/ballotmaker/cli.py
@@ -1,28 +1,51 @@
 """ The BallotMaker Command Line Interface (CLI)"""
-
+from pathlib import Path
 from typing import Optional
+from electos.ballotmaker.make_ballots import NO_ERRORS
 
 import typer
 
-from electos.ballotmaker import __version__
-
-PROGRAM_NAME = "BallotMaker"
-
-# for this to work, main needs to be a callback, not a command
-app = typer.Typer(no_args_is_help=True)
+from electos.ballotmaker import make_ballots
+from electos.ballotmaker.constants import NO_ERRORS, VERSION, PROGRAM_NAME
 
 
-@app.command()
-def cli(
-    version: Optional[bool] = typer.Option(
-        None, "--version", help="Print the version number."
-    ),
-):
-    """Create ballot PDFs from an Election Data File (EDF)"""
-    if version:
-        typer.echo(f"{PROGRAM_NAME} version: {__version__.__version__}")
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"{PROGRAM_NAME} version: {VERSION}")
         raise typer.Exit()
 
 
+# to display help when no arguments are provided,
+# main needs to be a callback, not a command
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None, "--version", callback=version_callback, help="Print the version number."
+    ),
+):
+    return NO_ERRORS
+
+
+@app.command()
+def make(
+    edf: Path = typer.Option(
+        ...,
+        help="EDF file with ballot data (JSON format)",
+    ),
+    output_dir: Path = typer.Option(
+        None, help="Output directory for generated PDFs (default: your home directory"
+    ),
+    settings: Path = typer.Option(None, help="Settings file for ballot generation"),
+):
+    """Make ballots from EDF file"""
+    make_ballots_result = make_ballots.make_ballots(edf, output_dir, settings)
+    # if make_ballots_result != NO_ERRORS:
+    #     typer.echo(f"Error: {make_ballots_result}")
+    return make_ballots_result
+
+
 if __name__ == "__main__":
-    app()
+    app()  # pragma: no cover
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,26 @@
+from electos.ballotmaker.make_ballots import NO_ERRORS
 from typer.testing import CliRunner
 from electos.ballotmaker import __version__, cli
+from electos.ballotmaker.constants import NO_ERRORS
 
 runner = CliRunner()
 
 
-def test_version_string():
-    assert __version__.__version__ == "0.1.0"
+# def test_version_string():
+#     assert __version__.__version__ == "0.1.0"
+
+
+def test_main():
+    main_result = cli.main()
+    assert main_result == NO_ERRORS
 
 
 def test_version():
     result = runner.invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert f"version: {__version__.__version__}" in result.stdout
+
+
+def test_make():
+    make_result = cli.make()
+    assert make_result == NO_ERRORS
