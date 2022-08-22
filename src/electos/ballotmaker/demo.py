f70c16bfec1204fdb3eab81616ce2dcb88e2cea5
Neil Johnson <neil@cadent.com>
2022-08-22 14:44:00 -0400

    Create demo ballot from the CLI

--- src/electos/ballotmaker/demo.py
+++ src/electos/ballotmaker/demo.py
@@ -1,6 +1,6 @@
 import logging
 
-# from electos.ballotmaker.ballots.demo_ballot import build_ballot
+from electos.ballotmaker.ballots.demo_ballot import build_ballot
 from electos.ballotmaker.constants import NO_ERRORS
 
 log = logging.getLogger(__name__)
@@ -8,5 +8,5 @@ log = logging.getLogger(__name__)
 
 def make_demo_ballot():
     log.debug("Starting ballotlab demo ...")
-    # build_ballot()
+    build_ballot()
     return NO_ERRORS
